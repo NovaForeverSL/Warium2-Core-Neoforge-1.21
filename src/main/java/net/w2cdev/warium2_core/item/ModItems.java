0d427dcb0532cf1a87277082ccbe063a74d9b434
Longy <76930542+NovaForeverSL@users.noreply.github.com>
2026-03-31 05:55:30 -0400

    Update ModItems.java

--- src/main/java/net/w2cdev/warium2_core/item/ModItems.java
+++ src/main/java/net/w2cdev/warium2_core/item/ModItems.java
@@ -12,56 +12,51 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public final class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Warium2_Core.MODID);
 
-    public static final DeferredItem<Item> ZINC = ITEMS.registerSimpleItem("zinc", new Item.Properties());
+    public static final DeferredItem<Item> INGOT_ZINC = ITEMS.registerSimpleItem("ingot_zinc", new Item.Properties());
     public static final DeferredItem<Item> RAW_ZINC = ITEMS.registerSimpleItem("raw_zinc", new Item.Properties());
 
-    public static final DeferredItem<Item> BERYLLIUM_INGOT = ITEMS.registerSimpleItem("beryllium_ingot", new Item.Properties());
+    public static final DeferredItem<Item> INGOT_BERYLLIUM = ITEMS.registerSimpleItem("ingot_beryllium", new Item.Properties());
     public static final DeferredItem<Item> RAW_BERYLLIUM = ITEMS.registerSimpleItem("raw_beryllium", new Item.Properties());
 
-    public static final DeferredItem<Item> LEAD_INGOT = ITEMS.registerSimpleItem("lead_ingot", new Item.Properties());
+    public static final DeferredItem<Item> INGOT_LEAD = ITEMS.registerSimpleItem("ingot_lead", new Item.Properties());
     public static final DeferredItem<Item> RAW_LEAD = ITEMS.registerSimpleItem("raw_lead", new Item.Properties());
 
-    public static final DeferredItem<Item> LITHIUM_INGOT = ITEMS.registerSimpleItem("lithium_ingot", new Item.Properties());
-    public static final DeferredItem<Item> RAW_LITHIUM = ITEMS.registerSimpleItem("raw_lithium", new Item.Properties());
-
-    public static final DeferredItem<Item> NICKEL_INGOT = ITEMS.registerSimpleItem("nickel_ingot", new Item.Properties());
+    public static final DeferredItem<Item> INGOT_NICKEL = ITEMS.registerSimpleItem("ingot_nickel", new Item.Properties());
     public static final DeferredItem<Item> RAW_NICKEL = ITEMS.registerSimpleItem("raw_nickel", new Item.Properties());
 
-    public static final DeferredItem<Item> URANIUM_INGOT = ITEMS.registerSimpleItem("uranium_ingot", new Item.Properties());
     public static final DeferredItem<Item> RAW_URANIUM = ITEMS.registerSimpleItem("raw_uranium", new Item.Properties());
 
-    public static final DeferredItem<BlockItem> BERYLLIUM_ORE = registerBlockItem("beryllium_ore", ModBlocks.BERYLLIUM_ORE);
-    public static final DeferredItem<BlockItem> BERYLLIUM_BLOCK = registerBlockItem("beryllium_block", ModBlocks.BERYLLIUM_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_BERYLLIUM_BLOCK = registerBlockItem("raw_beryllium_block", ModBlocks.RAW_BERYLLIUM_BLOCK);
+    public static final DeferredItem<BlockItem> ORE_BERYLLIUM = registerBlockItem("ore_beryllium", ModBlocks.ORE_BERYLLIUM);
+    public static final DeferredItem<BlockItem> ORE_BERYLLIUM_DEEPSLATE = registerBlockItem("ore_beryllium_deepslate", ModBlocks.ORE_BERYLLIUM_DEEPSLATE);
+    public static final DeferredItem<BlockItem> BLOCK_BERYLLIUM = registerBlockItem("block_beryllium", ModBlocks.BLOCK_BERYLLIUM);
+    public static final DeferredItem<BlockItem> RAW_BLOCK_BERYLLIUM = registerBlockItem("raw_block_beryllium", ModBlocks.RAW_BLOCK_BERYLLIUM);
 
-    public static final DeferredItem<BlockItem> LEAD_ORE = registerBlockItem("lead_ore", ModBlocks.LEAD_ORE);
-    public static final DeferredItem<BlockItem> DEEPSLATE_LEAD_ORE = registerBlockItem("deepslate_lead_ore", ModBlocks.DEEPSLATE_LEAD_ORE);
-    public static final DeferredItem<BlockItem> LEAD_BLOCK = registerBlockItem("lead_block", ModBlocks.LEAD_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_LEAD_BLOCK = registerBlockItem("raw_lead_block", ModBlocks.RAW_LEAD_BLOCK);
+    public static final DeferredItem<BlockItem> ORE_LEAD = registerBlockItem("ore_lead", ModBlocks.ORE_LEAD);
+    public static final DeferredItem<BlockItem> ORE_LEAD_DEEPSLATE = registerBlockItem("ore_lead_deepslate", ModBlocks.ORE_LEAD_DEEPSLATE);
+    public static final DeferredItem<BlockItem> BLOCK_LEAD = registerBlockItem("block_lead", ModBlocks.BLOCK_LEAD);
+    public static final DeferredItem<BlockItem> RAW_BLOCK_LEAD = registerBlockItem("raw_block_lead", ModBlocks.RAW_BLOCK_LEAD);
 
-    public static final DeferredItem<BlockItem> LITHIUM_ORE = registerBlockItem("lithium_ore", ModBlocks.LITHIUM_ORE);
-    public static final DeferredItem<BlockItem> LITHIUM_BLOCK = registerBlockItem("lithium_block", ModBlocks.LITHIUM_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_LITHIUM_BLOCK = registerBlockItem("raw_lithium_block", ModBlocks.RAW_LITHIUM_BLOCK);
+    public static final DeferredItem<BlockItem> ORE_NICKEL = registerBlockItem("ore_nickel", ModBlocks.ORE_NICKEL);
+    public static final DeferredItem<BlockItem> ORE_NICKEL_DEEPSLATE = registerBlockItem("ore_nickel_deepslate", ModBlocks.ORE_NICKEL_DEEPSLATE);
+    public static final DeferredItem<BlockItem> BLOCK_NICKEL = registerBlockItem("block_nickel", ModBlocks.BLOCK_NICKEL);
+    public static final DeferredItem<BlockItem> RAW_BLOCK_NICKEL = registerBlockItem("raw_block_nickel", ModBlocks.RAW_BLOCK_NICKEL);
 
-    public static final DeferredItem<BlockItem> NICKEL_ORE = registerBlockItem("nickel_ore", ModBlocks.NICKEL_ORE);
-    public static final DeferredItem<BlockItem> NICKEL_BLOCK = registerBlockItem("nickel_block", ModBlocks.NICKEL_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_NICKEL_BLOCK = registerBlockItem("raw_nickel_block", ModBlocks.RAW_NICKEL_BLOCK);
+    public static final DeferredItem<BlockItem> ORE_URANIUM = registerBlockItem("ore_uranium", ModBlocks.ORE_URANIUM);
+    public static final DeferredItem<BlockItem> ORE_URANIUM_DEEPSLATE = registerBlockItem("ore_uranium_deepslate", ModBlocks.ORE_URANIUM_DEEPSLATE);
+    public static final DeferredItem<BlockItem> RAW_BLOCK_URANIUM = registerBlockItem("raw_block_uranium", ModBlocks.RAW_BLOCK_URANIUM);
 
-    public static final DeferredItem<BlockItem> URANIUM_ORE = registerBlockItem("uranium_ore", ModBlocks.URANIUM_ORE);
-    public static final DeferredItem<BlockItem> URANIUM_BLOCK = registerBlockItem("uranium_block", ModBlocks.URANIUM_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_URANIUM_BLOCK = registerBlockItem("raw_uranium_block", ModBlocks.RAW_URANIUM_BLOCK);
-
-    public static final DeferredItem<BlockItem> ZINC_ORE = registerBlockItem("zinc_ore", ModBlocks.ZINC_ORE);
-    public static final DeferredItem<BlockItem> ZINC_BLOCK = registerBlockItem("zinc_block", ModBlocks.ZINC_BLOCK);
-    public static final DeferredItem<BlockItem> RAW_ZINC_BLOCK = registerBlockItem("raw_zinc_block", ModBlocks.RAW_ZINC_BLOCK);
+    public static final DeferredItem<BlockItem> ORE_ZINC = registerBlockItem("ore_zinc", ModBlocks.ORE_ZINC);
+    public static final DeferredItem<BlockItem> ORE_ZINC_DEEPSLATE = registerBlockItem("ore_zinc_deepslate", ModBlocks.ORE_ZINC_DEEPSLATE);
+    public static final DeferredItem<BlockItem> BLOCK_ZINC = registerBlockItem("block_zinc", ModBlocks.BLOCK_ZINC);
+    public static final DeferredItem<BlockItem> RAW_BLOCK_ZINC = registerBlockItem("raw_block_zinc", ModBlocks.RAW_BLOCK_ZINC);
 
     public static final DeferredItem<BlockItem> BAUXITE = registerBlockItem("bauxite", ModBlocks.BAUXITE);
-    public static final DeferredItem<BlockItem> REINFORCED_CONCRETE = registerBlockItem("reinforced_concrete", ModBlocks.REINFORCED_CONCRETE);
-    public static final DeferredItem<BlockItem> CRACKED_CONCRETE = registerBlockItem("cracked_concrete", ModBlocks.CRACKED_CONCRETE);
-    public static final DeferredItem<BlockItem> DAMAGED_CONCRETE = registerBlockItem("damaged_concrete", ModBlocks.DAMAGED_CONCRETE);
-    public static final DeferredItem<BlockItem> FRACTURED_CONCRETE = registerBlockItem("fractured_concrete", ModBlocks.FRACTURED_CONCRETE);
-    public static final DeferredItem<BlockItem> DESTROYED_CONCRETE = registerBlockItem("destroyed_concrete", ModBlocks.DESTROYED_CONCRETE);
-    public static final DeferredItem<BlockItem> OVERGROWN_REINFORCED_CONCRETE = registerBlockItem("overgrown_reinforced_concrete", ModBlocks.OVERGROWN_REINFORCED_CONCRETE);
+    public static final DeferredItem<BlockItem> CEMENTITE_REINFORCED = registerBlockItem("cementite_reinforced", ModBlocks.CEMENTITE_REINFORCED);
+    public static final DeferredItem<BlockItem> CEMENTITE_CRACKED = registerBlockItem("cementite_cracked", ModBlocks.CEMENTITE_CRACKED);
+    public static final DeferredItem<BlockItem> CEMENTITE_DAMAGED = registerBlockItem("cementite_damaged", ModBlocks.CEMENTITE_DAMAGED);
+    public static final DeferredItem<BlockItem> CEMENTITE_FRACTURED = registerBlockItem("cementite_fractured", ModBlocks.CEMENTITE_FRACTURED);
+    public static final DeferredItem<BlockItem> CEMENTITE_DESTROYED = registerBlockItem("cementite_destroyed", ModBlocks.CEMENTITE_DESTROYED);
+    public static final DeferredItem<BlockItem> CEMENTITE_OVERGROWN = registerBlockItem("cementite_overgrown", ModBlocks.CEMENTITE_OVERGROWN);
     public static final DeferredItem<BlockItem> REBAR = registerBlockItem("rebar", ModBlocks.REBAR);
 
     private ModItems() {
